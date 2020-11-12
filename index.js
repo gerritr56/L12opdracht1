523f79f84c5ab5174217e000b6f008285bbeb9ea
gerritr56 <68347062+gerritr56@users.noreply.github.com>
2020-11-12 13:04:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let name = 'Gerrit';
+console.log(name);
+
+let additionFirst = 4;
+let additionSecond = 4;
+let addition = additionFirst + additionSecond;
+console.log(addition);
+
+let substractionFirst = 8;
+let substractionSecond = 2;
+let substraction = substractionFirst - substractionSecond;
+console.log(substraction);
+
+let multiplicationFirst = 8;
+let multiplicationSecond = 2;
+let multiplication = multiplicationFirst * multiplicationSecond;
+console.log(multiplication);
+
+let divisionFirst = 8;
+let divisionSecond = 2;
+let division = divisionFirst / divisionSecond;
+console.log(division);
+
+let modulusFirst = 8;
+let modulusSecond = 2;
+let modulus = modulusFirst / modulusSecond;
+console.log(modulus);
+
+let age = 64;
+console.log(age);
+console.log(typeof age);
+
+let ageText = 'vierenzestig';
+console.log(ageText);
+console.log(typeof ageText);
